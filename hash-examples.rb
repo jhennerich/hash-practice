963351165b58355f209b14d81103d3f613633aaf
John Hennerich <john.hennerich@gmail.com>
2021-11-09 20:44:12 -0700

    Add hashs

--- hash-examples.rb
+++ hash-examples.rb
@@ -5,3 +5,17 @@ car = {
   model: "Ranger",
   year: 1998
 }
+
+#animal hash with name, color
+animal = {
+  name: "Bear",
+  color: "Black"
+}
+
+#customer hash with name, phone_number, email_address
+
+customer = {
+  name: "John",
+  phone_number: 3035503431,
+  email_address: "john.hennerich@gmail.com"
+}
